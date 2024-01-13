d75560ffcd2c307713039697010faa923e9b97a7
1nv8rZim <msf9542@rit.edu>
2024-01-13 16:42:54 -0500

    daisyui setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import "./index.css";
 
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <div className='flex flex-col justify-center container mx-auto'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <button className='btn btn-large btn-primary'>Click Me</button>
+    </div>
+  );
 }
