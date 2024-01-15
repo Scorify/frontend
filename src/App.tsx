2ea54ae687ef228b91dba5840a1cfd39e2e9b5f9
1nv8rZim <msf9542@rit.edu>
2024-01-14 22:24:17 -0500

    Add lazy loading and routing to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,65 @@
+import React, { Suspense } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import "./index.css";
 
+const LazyComponent = ({
+  element,
+}: {
+  element: React.ReactNode;
+}): React.ReactElement => {
+  return <Suspense fallback={<>Loading...</>}>{element}</Suspense>;
+};
+
 export default function App() {
-  return (
-    <div className='flex flex-col justify-center container mx-auto'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-      <button className='btn btn-large btn-primary'>Click Me</button>
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <LazyComponent
+          element={
+            <>
+              <Navbar />
+              <div className='container mx-auto p-4'>
+                <h1 className='text-4xl font-bold mb-4'>Hello, DaisyUI!</h1>
+                <button className='btn btn-primary'>Click me</button>
+              </div>
+            </>
+          }
+        />
+      ),
+    },
+    {
+      path: "/home",
+      element: (
+        <LazyComponent
+          element={
+            <>
+              <Navbar />
+              <div className='container mx-auto p-4'>
+                <h1 className='text-4xl font-bold mb-4'>Home</h1>
+              </div>
+            </>
+          }
+        />
+      ),
+    },
+    {
+      path: "/about",
+      element: (
+        <LazyComponent
+          element={
+            <>
+              <Navbar />
+              <div className='container mx-auto p-4'>
+                <h1 className='text-4xl font-bold mb-4'>About</h1>
+              </div>
+            </>
+          }
+        />
+      ),
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
