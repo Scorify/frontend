ff1f1881f44f1204850b8352cc6550ffcfff2f85
1nv8rZim <msf9542@rit.edu>
2024-01-16 22:56:07 -0500

    Refactor code structure and update UI layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,6 +2,7 @@ import { Dispatch, SetStateAction, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Drawer from "./Drawer";
+import { Container, Box } from "@mui/material";
 
 type props = {
   theme: string;
@@ -12,14 +13,16 @@ export default function Main({ theme, setTheme }: props) {
   const [drawerState, setDrawerState] = useState(false);
 
   return (
-    <>
+    <Box sx={{ minHeight: "100vh", backgroundColor: "default" }}>
       <Drawer drawerState={drawerState} setDrawerState={setDrawerState} />
       <Navbar
         theme={theme}
         setTheme={setTheme}
         setDrawerState={setDrawerState}
       />
-      <Outlet />
-    </>
+      <Container component='main'>
+        <Outlet />
+      </Container>
+    </Box>
   );
 }
