deefa7ee8839c5729300c325f2a2748616f71568
1nv8rZim <msf9542@rit.edu>
2024-01-17 23:33:37 -0500

    Add login functionality and set authentication cookie

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,52 @@
+import { useMutation } from "@apollo/client";
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { enqueueSnackbar } from "notistack";
+import { CookieSetOptions } from "universal-cookie";
 
 import { PasswordInput } from "../components";
+import { LOGIN } from "../queries";
 
-export default function Login() {
+type props = {
+  setCookie: (
+    name: "auth",
+    value: any,
+    options?: CookieSetOptions | undefined
+  ) => void;
+};
+
+export default function Login({ setCookie }: props) {
+  const [loginMutation, {}] = useMutation(LOGIN);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    loginMutation({
+      variables: {
+        username: data.get("username"),
+        password: data.get("password"),
+      },
+    })
+      .then((response) => {
+        if (response.data.login) {
+          enqueueSnackbar("Logged in successfully", { variant: "success" });
+
+          setCookie("auth", response.data.login.token, {
+            path: response.data.login.path,
+            domain: response.data.login.domain,
+            expires: new Date(response.data.login.expires * 1000),
+            secure: response.data.login.secure,
+            httpOnly: response.data.login.httpOnly,
+          });
+
+          document.location.href = "/";
+        }
+      })
+      .catch((error) => {
+        enqueueSnackbar("Invalid username or password", { variant: "error" });
+        console.log(error);
+      });
+  };
+
   return (
     <Container component='main' maxWidth='xs'>
       <Box
@@ -16,12 +60,7 @@ export default function Login() {
         <Typography component='h1' variant='h3'>
           Sign in
         </Typography>
-        <Box
-          component='form'
-          onSubmit={() => {
-            alert("TODO: Implement login");
-          }}
-        >
+        <Box component='form' onSubmit={handleSubmit}>
           <TextField
             margin='normal'
             required
