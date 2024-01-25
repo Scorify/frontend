5271403d1b4e06d83e8d5600fb63d51c569723a0
1nv8rZim <msf9542@rit.edu>
2024-01-24 22:47:24 -0500

    Remove NotFound import from index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,5 @@ import ChangePassword from "./ChangePassword";
 import Home from "./Home";
 import Login from "./Login";
 import Me from "./Me";
-import NotFound from "./NotFound";
 
-export { ChangePassword, Home, Login, Me, NotFound };
+export { ChangePassword, Home, Login, Me };
