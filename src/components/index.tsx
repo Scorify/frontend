6d25dbd9fd078fe390f9fdeb14cd6b52e41e849d
1nv8rZim <msf9542@rit.edu>
2024-01-24 22:47:40 -0500

    Add Admin and Error components to index.tsx

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,8 @@
+import Admin from "./Admin";
 import DrawerComponent from "./Drawer";
+import Error from "./Error";
 import Main from "./Main";
 import Navbar from "./Navbar";
 import PasswordInput from "./PasswordInput";
 
-export { DrawerComponent as Drawer, Main, Navbar, PasswordInput };
+export { Admin, DrawerComponent as Drawer, Error, Main, Navbar, PasswordInput };
