4a349c11f7b1c215793a304454bf0a7a94a3f031
1nv8rZim <msf9542@rit.edu>
2024-01-25 19:28:46 -0500

    Add new components and page for Checks

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,19 @@
 import Admin from "./Admin";
+import ConfigField from "./ConfigField";
+import CreateCheckModal from "./CreateCheckModal";
 import DrawerComponent from "./Drawer";
 import Error from "./Error";
 import Main from "./Main";
 import Navbar from "./Navbar";
 import PasswordInput from "./PasswordInput";
 
-export { Admin, DrawerComponent as Drawer, Error, Main, Navbar, PasswordInput };
+export {
+  Admin,
+  ConfigField,
+  CreateCheckModal,
+  DrawerComponent as Drawer,
+  Error,
+  Main,
+  Navbar,
+  PasswordInput,
+};
