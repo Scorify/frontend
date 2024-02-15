3af73d49a9d4eba60b3e3c6c3b1f83d7299a3411
1nv8rZim <msf9542@rit.edu>
2024-02-14 22:31:34 -0500

    Add ConfigureCheck component

---
+++ src/components/User/ConfigureCheck.tsx
@@ -0,0 +1,160 @@
+import { useMemo, useState } from "react";
+
+import { ExpandMore } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Collapse,
+  Divider,
+  Grow,
+  IconButton,
+  Slide,
+  Typography,
+} from "@mui/material";
+
+import { enqueueSnackbar } from "notistack";
+import { ConfigField } from "..";
+import { ConfigsQuery, useEditConfigMutation } from "../../graph";
+
+type props = {
+  config: ConfigsQuery["configs"][0];
+  handleRefetch: () => void;
+  visible: boolean;
+};
+
+export default function ConfigureCheck({
+  config,
+  visible,
+  handleRefetch,
+}: props) {
+  const [expanded, setExpanded] = useState(false);
+
+  const [checkConfig, setCheckConfig] = useState<{
+    [key: string]: number | boolean | string;
+  }>(JSON.parse(config.config));
+  const configChanged = useMemo(
+    () => JSON.stringify(checkConfig) != config.config,
+    [checkConfig, config.config]
+  );
+
+  const [useEditConfig] = useEditConfigMutation({
+    onCompleted: () => {
+      enqueueSnackbar("Config saved", { variant: "success" });
+      handleRefetch();
+    },
+    onError: (error) => {
+      enqueueSnackbar("Error saving config", { variant: "error" });
+      console.error(error);
+    },
+  });
+
+  const handleConfigChange = (
+    key: string,
+    value: string | number | boolean
+  ) => {
+    setCheckConfig({ ...checkConfig, [key]: value });
+  };
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const handleSave = () => {
+    useEditConfig({
+      variables: {
+        id: config.id,
+        config: JSON.stringify(checkConfig),
+      },
+    });
+  };
+
+  return (
+    <Grow in={true}>
+      <Card
+        sx={{
+          width: "100%",
+          marginBottom: "24px",
+          display: visible ? "block" : "none",
+        }}
+        variant='elevation'
+      >
+        <CardHeader
+          title={
+            <Box display='flex' flexDirection='row' alignItems='baseline'>
+              <Typography variant='h6' component='div' marginRight='24px'>
+                {config.check.name}
+              </Typography>
+              <Typography
+                variant='subtitle1'
+                color='textSecondary'
+                component='div'
+              >
+                {config.check.source.name}
+              </Typography>
+            </Box>
+          }
+          action={
+            <Box display='flex' flexDirection='row' gap='12px'>
+              <IconButton>
+                <ExpandMore />
+              </IconButton>
+              <Slide
+                in={configChanged}
+                timeout={300}
+                style={{
+                  transformOrigin: "right",
+                }}
+                direction='left'
+                unmountOnExit
+                mountOnEnter
+              >
+                <Button
+                  variant='contained'
+                  onClick={(e) => {
+                    if (!expanded) {
+                      e.stopPropagation();
+                    }
+
+                    handleSave();
+                  }}
+                >
+                  Save
+                </Button>
+              </Slide>
+            </Box>
+          }
+          onClick={handleExpandClick}
+        />
+        {expanded && <Divider sx={{ margin: "0px 20%" }} />}
+
+        <Collapse in={expanded} timeout={300}>
+          <CardContent>
+            <Box
+              sx={{
+                display: "flex",
+                gap: "16px",
+                flexWrap: "wrap",
+                justifyContent: "center",
+              }}
+            >
+              {Object.entries(JSON.parse(config.check.source.schema)).map(
+                ([index, type]) => (
+                  <ConfigField
+                    key={index}
+                    index={index}
+                    handleInputChange={handleConfigChange}
+                    value={type as "string" | "int" | "bool"}
+                    config={checkConfig}
+                  />
+                )
+              )}
+            </Box>
+          </CardContent>
+        </Collapse>
+      </Card>
+    </Grow>
+  );
+}
--- src/graph/index.ts
+++ src/graph/index.ts
@@ -35,7 +35,7 @@ export type CheckConfiguration = {
 export type Config = {
   __typename?: 'Config';
   check: Check;
-  config: CheckConfiguration;
+  config: Scalars['JSON']['output'];
   id: Scalars['ID']['output'];
   user: User;
 };
@@ -79,7 +79,7 @@ export type MutationChangePasswordArgs = {
 
 
 export type MutationCreateCheckArgs = {
-  config: Scalars['String']['input'];
+  config: Scalars['JSON']['input'];
   editable_fields: Array<Scalars['String']['input']>;
   name: Scalars['String']['input'];
   source: Scalars['String']['input'];
@@ -105,7 +105,7 @@ export type MutationDeleteUserArgs = {
 
 
 export type MutationEditConfigArgs = {
-  config: Scalars['String']['input'];
+  config: Scalars['JSON']['input'];
   id: Scalars['ID']['input'];
 };
 
@@ -371,7 +371,7 @@ export type ChecksLazyQueryHookResult = ReturnType<typeof useChecksLazyQuery>;
 export type ChecksSuspenseQueryHookResult = ReturnType<typeof useChecksSuspenseQuery>;
 export type ChecksQueryResult = Apollo.QueryResult<ChecksQuery, ChecksQueryVariables>;
 export const CreateCheckDocument = gql`
-    mutation CreateCheck($name: String!, $source: String!, $config: String!, $editable_fields: [String!]!) {
+    mutation CreateCheck($name: String!, $source: String!, $config: JSON!, $editable_fields: [String!]!) {
   createCheck(
     name: $name
     source: $source
@@ -754,16 +754,13 @@ export const ConfigsDocument = gql`
   configs {
     id
     check {
-      id
       name
       source {
         name
         schema
       }
     }
-    config {
-      config
-    }
+    config
   }
 }
     `;
@@ -799,6 +796,40 @@ export type ConfigsQueryHookResult = ReturnType<typeof useConfigsQuery>;
 export type ConfigsLazyQueryHookResult = ReturnType<typeof useConfigsLazyQuery>;
 export type ConfigsSuspenseQueryHookResult = ReturnType<typeof useConfigsSuspenseQuery>;
 export type ConfigsQueryResult = Apollo.QueryResult<ConfigsQuery, ConfigsQueryVariables>;
+export const EditConfigDocument = gql`
+    mutation EditConfig($id: ID!, $config: JSON!) {
+  editConfig(id: $id, config: $config) {
+    id
+  }
+}
+    `;
+export type EditConfigMutationFn = Apollo.MutationFunction<EditConfigMutation, EditConfigMutationVariables>;
+
+/**
+ * __useEditConfigMutation__
+ *
+ * To run a mutation, you first call `useEditConfigMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useEditConfigMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [editConfigMutation, { data, loading, error }] = useEditConfigMutation({
+ *   variables: {
+ *      id: // value for 'id'
+ *      config: // value for 'config'
+ *   },
+ * });
+ */
+export function useEditConfigMutation(baseOptions?: Apollo.MutationHookOptions<EditConfigMutation, EditConfigMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<EditConfigMutation, EditConfigMutationVariables>(EditConfigDocument, options);
+      }
+export type EditConfigMutationHookResult = ReturnType<typeof useEditConfigMutation>;
+export type EditConfigMutationResult = Apollo.MutationResult<EditConfigMutation>;
+export type EditConfigMutationOptions = Apollo.BaseMutationOptions<EditConfigMutation, EditConfigMutationVariables>;
 export type MeQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -828,7 +859,7 @@ export type ChecksQuery = { __typename?: 'Query', checks: Array<{ __typename?: '
 export type CreateCheckMutationVariables = Exact<{
   name: Scalars['String']['input'];
   source: Scalars['String']['input'];
-  config: Scalars['String']['input'];
+  config: Scalars['JSON']['input'];
   editable_fields: Array<Scalars['String']['input']> | Scalars['String']['input'];
 }>;
 
@@ -907,4 +938,12 @@ export type AdminLoginMutation = { __typename?: 'Mutation', adminLogin: { __type
 export type ConfigsQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type ConfigsQuery = { __typename?: 'Query', configs: Array<{ __typename?: 'Config', id: string, check: { __typename?: 'Check', id: string, name: string, source: { __typename?: 'Source', name: string, schema: string } }, config: { __typename?: 'CheckConfiguration', config: any } }> };
+export type ConfigsQuery = { __typename?: 'Query', configs: Array<{ __typename?: 'Config', id: string, config: any, check: { __typename?: 'Check', name: string, source: { __typename?: 'Source', name: string, schema: string } } }> };
+
+export type EditConfigMutationVariables = Exact<{
+  id: Scalars['ID']['input'];
+  config: Scalars['JSON']['input'];
+}>;
+
+
+export type EditConfigMutation = { __typename?: 'Mutation', editConfig: { __typename?: 'Config', id: string } };
