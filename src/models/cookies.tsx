8d4f1cf147f2d4248dc07baa2ec0047340ac2460
1nv8rZim <msf9542@rit.edu>
2024-03-08 22:41:32 -0500

    Add cookies model and types

---
+++ src/models/cookies.tsx
@@ -0,0 +1,18 @@
+import { CookieSetOptions } from "universal-cookie";
+
+type Cookies = {
+  auth?: any;
+};
+
+type SetCookie = (
+  name: "auth",
+  value: any,
+  options?: CookieSetOptions | undefined
+) => void;
+
+type RemoveCookie = (
+  name: "auth",
+  options?: CookieSetOptions | undefined
+) => void;
+
+export type { Cookies, SetCookie, RemoveCookie };
