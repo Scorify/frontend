62f388ca9ac0e5dd538ba6cb1a7972588fe937dd
1nv8rZim <msf9542@rit.edu>
2024-03-09 23:04:35 -0500

    Add updateCookie and refetchMe to AuthContext

--- src/components/Context/AuthProvider.tsx
+++ src/components/Context/AuthProvider.tsx
@@ -1,10 +1,30 @@
-import { MeQuery } from "../../graph";
-import { Cookies, JWT, RemoveCookie, SetCookie } from "../../models";
+import { createContext } from "react";
 
-export interface AuthContextType {
+import { MeQuery } from "../../graph";
+import {
+  Cookies,
+  JWT,
+  RemoveCookie,
+  SetCookie,
+  UpdateCookie,
+} from "../../models";
+
+interface AuthContextType {
   jwt: JWT;
   cookies: Cookies;
   setCookie: SetCookie;
   removeCookie: RemoveCookie;
+  updateCookie: UpdateCookie;
   me: MeQuery | undefined;
+  refetchMe: () => void;
 }
+
+export const AuthContext = createContext<AuthContextType>({
+  jwt: undefined,
+  cookies: {},
+  setCookie: () => {},
+  removeCookie: () => {},
+  updateCookie: () => {},
+  me: undefined,
+  refetchMe: () => {},
+});
