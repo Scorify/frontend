977e61099933e69f639e9829429b0db910bc5b9c
1nv8rZim <msf9542@rit.edu>
2024-03-10 21:36:19 -0400

    Add UseAuth hook and remove UseJWT hook

---
+++ src/hooks/UseAuth.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useMemo } from "react";
+
+import { ApolloClient, NormalizedCacheObject } from "@apollo/client";
+import { jwtDecode } from "jwt-decode";
+import { useCookies } from "react-cookie";
+
+import { MeQuery, useMeQuery } from "../graph";
+import { Cookies, JWT, RemoveCookie, SetCookie, UpdateCookie } from "../models";
+
+export function useAuth(apolloClient: ApolloClient<NormalizedCacheObject>): {
+  jwt: JWT | undefined;
+  me: MeQuery | undefined;
+  refetchMe: () => void;
+  cookies: Cookies;
+  setCookie: SetCookie;
+  removeCookie: RemoveCookie;
+  updateCookie: UpdateCookie;
+} {
+  const [cookies, setCookie, removeCookie, updateCookie] = useCookies(["auth"]);
+
+  const { data: me, refetch: refetchMe } = useMeQuery({
+    onError: (error) => console.error(error),
+  });
+
+  useEffect(() => {
+    // sleep(250);
+    apolloClient.clearStore().then(() => {
+      refetchMe();
+    });
+  }, [cookies.auth]);
+
+  const jwt = useMemo(
+    () => (cookies?.auth ? (jwtDecode(cookies?.auth) as JWT) : undefined),
+    [cookies?.auth]
+  );
+
+  return {
+    jwt,
+    me,
+    refetchMe,
+    cookies,
+    setCookie,
+    removeCookie,
+    updateCookie,
+  };
+}
