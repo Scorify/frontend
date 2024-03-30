41ce91223cdca5ec78ef6f04f9c322f1e15d7a64
1nv8rZim <msf9542@rit.edu>
2024-03-29 23:39:42 -0400

    Add new dependencies and update ScoreboardPage component

--- src/pages/Scoreboard/Scoreboard.tsx
+++ src/pages/Scoreboard/Scoreboard.tsx
@@ -1,4 +1,13 @@
-import { Box, Container, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Typography,
+} from "@mui/material";
+
+import { useEffect, useMemo } from "react";
+import { StatusEnum, useScoreboardQuery } from "../../graph";
 
 import { Scoreboard } from "../../components";
 import { NormalScoreboardTheme } from "../../constants";
@@ -64,6 +73,25 @@ type props = {
 };
 
 export default function ScoreboardPage({ theme }: props) {
+  const { data, error, loading, refetch } = useScoreboardQuery();
+
+  useEffect(() => {
+    refetch();
+    refetch();
+  }, []);
+
+  const scoreboardData = useMemo(() => {
+    return {
+      top: data?.scoreboard.teams.map((team) => team.number) ?? [],
+      left: data?.scoreboard.checks.map((check) => check.name) ?? [],
+      values: data?.scoreboard.statuses.map((check_statuses) =>
+        check_statuses.map((statuses) =>
+          statuses.status == StatusEnum.Up ? 1 : 0 ?? 0
+        )
+      ) ?? [[]],
+    };
+  }, [data]);
+
   return (
     <Container component='main' maxWidth='xl'>
       <Box
@@ -83,13 +111,18 @@ export default function ScoreboardPage({ theme }: props) {
         >
           Scoreboard
         </Typography>
+        <Button onClick={() => refetch()}>dfsdfsd</Button>
         <Box m={2} />
-        <Scoreboard
-          theme={theme}
-          scoreboardData={scoreboardData}
-          scoreboardTheme={NormalScoreboardTheme}
-          cornerLabel='Team'
-        />
+        {error && <Typography variant='h6'>Error: {error.message}</Typography>}
+        {loading && !data && <CircularProgress />}
+        {data && (
+          <Scoreboard
+            theme={theme}
+            scoreboardData={scoreboardData}
+            scoreboardTheme={NormalScoreboardTheme}
+            cornerLabel='Team'
+          />
+        )}
       </Box>
     </Container>
   );
