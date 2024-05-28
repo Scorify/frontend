aef8d00a01ffea3b68b03a3bde6c0f61e434a9c7
1nv8rZim <msf9542@rit.edu>
2024-05-27 22:08:59 -0400

    Refactor Drawer to use reusable "DrawerItem" component

--- src/components/Core/Drawer.tsx
+++ src/components/Core/Drawer.tsx
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router";
 import {
   AdminPanelSettings,
   EditNote,
-  Scoreboard,
   Group,
   Home,
   KeyboardReturn,
   Login,
   Logout,
   Password,
+  Scoreboard,
   Vaccines,
 } from "@mui/icons-material";
 import {
@@ -27,6 +27,23 @@ import {
 import { MeQuery } from "../../graph";
 import { JWT } from "../../models";
 import { RemoveCookie } from "../../models/cookies";
+
+type drawerItemProps = {
+  label: string;
+  icon: JSX.Element;
+  onClick?: () => void;
+};
+
+function DrawerItem({ label, icon, onClick }: drawerItemProps) {
+  return (
+    <ListItem disablePadding onClick={onClick}>
+      <ListItemButton>
+        <ListItemIcon>{icon}</ListItemIcon>
+        <ListItemText primary={label} />
+      </ListItemButton>
+    </ListItem>
+  );
+}
 
 type props = {
   drawerState: boolean;
@@ -68,155 +85,90 @@ export default function DrawerComponent({
         onKeyDown={toggleDrawer(false)}
       >
         <List>
-          <ListItem
-            disablePadding
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            <ListItemButton>
-              <ListItemIcon>{<Home />}</ListItemIcon>
-              <ListItemText primary='Home' />
-            </ListItemButton>
-          </ListItem>
-          <ListItem
-            disablePadding
-            onClick={() => {
-              navigate("/scoreboard");
-            }}
-          >
-            <ListItemButton>
-              <ListItemIcon>{<Scoreboard />}</ListItemIcon>
-              <ListItemText primary='Scoreboard' />
-            </ListItemButton>
-          </ListItem>
+          <DrawerItem
+            label='Home'
+            icon={<Home />}
+            onClick={() => navigate("/")}
+          />
+          <DrawerItem
+            label='Scoreboard'
+            icon={<Scoreboard />}
+            onClick={() => navigate("/scoreboard")}
+          />
         </List>
         <Divider />
         {me?.me ? (
           <>
             <List>
               {jwt?.become ? (
-                <ListItem disablePadding onClick={returnToAdmin}>
-                  <ListItemButton>
-                    <ListItemIcon>{<KeyboardReturn />}</ListItemIcon>
-                    <ListItemText primary='Return to Admin User' />
-                  </ListItemButton>
-                </ListItem>
+                <DrawerItem
+                  label='Return to Admin'
+                  icon={<KeyboardReturn />}
+                  onClick={returnToAdmin}
+                />
               ) : (
-                <ListItem
-                  disablePadding
+                <DrawerItem
+                  label='Logout'
+                  icon={<Logout />}
                   onClick={() => {
                     removeCookie("auth");
                     navigate("/");
                   }}
-                >
-                  <ListItemButton>
-                    <ListItemIcon>{<Logout />}</ListItemIcon>
-                    <ListItemText primary='Logout' />
-                  </ListItemButton>
-                </ListItem>
+                />
               )}
-              <ListItem
-                disablePadding
-                onClick={() => {
-                  navigate("/password");
-                }}
-              >
-                <ListItemButton>
-                  <ListItemIcon>{<Password />}</ListItemIcon>
-                  <ListItemText primary='Change Password' />
-                </ListItemButton>
-              </ListItem>
+              <DrawerItem
+                label='Change Password'
+                icon={<Password />}
+                onClick={() => navigate("/password")}
+              />
             </List>
             <Divider />
             {me.me.role === "user" && (
               <List>
-                <ListItem
-                  disablePadding
-                  onClick={() => {
-                    navigate("/checks");
-                  }}
-                >
-                  <ListItemButton>
-                    <ListItemIcon>{<EditNote />}</ListItemIcon>
-                    <ListItemText primary='Checks' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem
-                  disablePadding
-                  onClick={() => {
-                    navigate("/injects");
-                  }}
-                >
-                  <ListItemButton>
-                    <ListItemIcon>{<Vaccines />}</ListItemIcon>
-                    <ListItemText primary='Injects' />
-                  </ListItemButton>
-                </ListItem>
+                <DrawerItem
+                  label='Checks'
+                  icon={<EditNote />}
+                  onClick={() => navigate("/checks")}
+                />
+                <DrawerItem
+                  label='Injects'
+                  icon={<Vaccines />}
+                  onClick={() => navigate("/injects")}
+                />
               </List>
             )}
             {me.me.role === "admin" && (
               <List>
-                <ListItem
-                  disablePadding
-                  onClick={() => {
-                    navigate("/admin");
-                  }}
-                >
-                  <ListItemButton>
-                    <ListItemIcon>{<AdminPanelSettings />}</ListItemIcon>
-                    <ListItemText primary='Admin' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem
-                  disablePadding
-                  onClick={() => {
-                    navigate("/admin/injects");
-                  }}
-                >
-                  <ListItemButton>
-                    <ListItemIcon>{<Vaccines />}</ListItemIcon>
-                    <ListItemText primary='Injects' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem
-                  disablePadding
-                  onClick={() => {
-                    navigate("/admin/checks");
-                  }}
-                >
-                  <ListItemButton>
-                    <ListItemIcon>{<EditNote />}</ListItemIcon>
-                    <ListItemText primary='Checks' />
-                  </ListItemButton>
-                </ListItem>
-                <ListItem
-                  disablePadding
-                  onClick={() => {
-                    navigate("/admin/users");
-                  }}
-                >
-                  <ListItemButton>
-                    <ListItemIcon>{<Group />}</ListItemIcon>
-                    <ListItemText primary='Users' />
-                  </ListItemButton>
-                </ListItem>
+                <DrawerItem
+                  label='Admin'
+                  icon={<AdminPanelSettings />}
+                  onClick={() => navigate("/admin")}
+                />
+                <DrawerItem
+                  label='Injects'
+                  icon={<Vaccines />}
+                  onClick={() => navigate("/admin/injects")}
+                />
+                <DrawerItem
+                  label='Checks'
+                  icon={<EditNote />}
+                  onClick={() => navigate("/admin/checks")}
+                />
+                <DrawerItem
+                  label='Users'
+                  icon={<Group />}
+                  onClick={() => navigate("/admin/users")}
+                />
               </List>
             )}
           </>
         ) : (
           <List>
-            <ListItem
-              disablePadding
-              onClick={() => {
-                navigate("/login");
-              }}
-            >
-              <ListItemButton>
-                <ListItemIcon>{<Login />}</ListItemIcon>
-                <ListItemText primary='Login' />
-              </ListItemButton>
-            </ListItem>
+            <DrawerItem
+              label='Login'
+              icon={<Login />}
+              onClick={() => navigate("/login")}
+            />
           </List>
         )}
       </Box>
