dcc391e4bef2f07c10d006b36c4986e32c8b36e4
1nv8rZim <msf9542@rit.edu>
2024-06-24 20:01:33 -0400

    feat: Update User component to handle unauthorized access

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,22 +1,18 @@
-import { useContext, useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-import { Error, Loading } from "..";
+import { Error } from "..";
 import { Role } from "../../graph";
 import { AuthContext } from "../Context";
 
 export default function User() {
-  const { me, meLoading, meError } = useContext(AuthContext);
+  const { me } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (meError) {
-      navigate("/login");
-    }
-  }, [meError, navigate]);
-
-  if (!me || meLoading) {
-    return <Loading />;
+  if (!me) {
+    navigate("/login");
+    return <Error code={401} message='Unauthorized' />;
   }
 
   if (me.me?.role !== Role.User) {
