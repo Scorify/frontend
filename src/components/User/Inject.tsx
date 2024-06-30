8b7869a56336d62014952d097d82cdd33fda4977
1nv8rZim <msf9542@rit.edu>
2024-06-29 22:45:58 -0400

    refactor: Update components to use Dropdown for collapsible sections and lazy loading content

--- src/components/User/Inject.tsx
+++ src/components/User/Inject.tsx
@@ -1,27 +1,12 @@
 import { useEffect, useState } from "react";
 
-import { ExpandLess, ExpandMore } from "@mui/icons-material";
-import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  CardHeader,
-  Chip,
-  Collapse,
-  Divider,
-  Grow,
-  IconButton,
-  Paper,
-  TextField,
-  Slide,
-  Typography,
-} from "@mui/material";
+import { Box, Button, Chip, Paper, TextField, Typography } from "@mui/material";
 
 import { InjectsQuery } from "../../graph";
+import Dropdown from "../Common/Dropdown";
+import FileChip from "../Common/FileChip";
 import Submission from "./Submission";
 import SubmitInjectModal from "./SubmitInjectModal";
-import FileChip from "../Common/FileChip";
 
 type countdownChipProps = {
   target: number;
@@ -78,8 +63,6 @@ export default function Inject({ handleRefetch, inject, visible }: props) {
   const [expanded, setExpanded] = useState(false);
   const [open, setOpen] = useState(false);
 
-  const handleExpandClick = () => setExpanded((prev) => !prev);
-
   const sortedSubmissions = inject.submissions
     .filter((submission) => submission.graded)
     .sort(
@@ -95,245 +78,203 @@ export default function Inject({ handleRefetch, inject, visible }: props) {
     );
 
   return (
-    <>
-      <SubmitInjectModal
-        inject={inject}
-        handleRefetch={handleRefetch}
-        open={open}
-        setOpen={setOpen}
-      />
-      <Grow in={true}>
-        <Card
-          sx={{
-            width: "100%",
-            marginBottom: "24px",
-            display: visible ? "block" : "none",
+    <Dropdown
+      modal={
+        <SubmitInjectModal
+          inject={inject}
+          handleRefetch={handleRefetch}
+          open={open}
+          setOpen={setOpen}
+        />
+      }
+      title={
+        <>
+          <Typography variant='h6' component='div' marginRight='24px'>
+            {inject.title}
+          </Typography>
+          <CountdownChip target={new Date(inject.end_time).getTime()} />
+          {sortedSubmissions.length > 0 && (
+            <Chip
+              label={`Score: ${
+                sortedSubmissions[0].rubric?.fields.reduce(
+                  (acc, field) => acc + field.score,
+                  0
+                ) ?? 0
+              }/${inject.rubric.max_score}`}
+              color='success'
+              size='small'
+              sx={{ marginLeft: "12px" }}
+            />
+          )}
+        </>
+      }
+      expandableButtons={[
+        <Button
+          variant='contained'
+          color='success'
+          onClick={(e) => {
+            if (!expanded) {
+              e.stopPropagation();
+            }
+
+            setOpen(true);
           }}
-          variant='elevation'
         >
-          <CardHeader
-            title={
-              <Box display='flex' flexDirection='row' alignItems='center'>
-                <Typography variant='h6' component='div' marginRight='24px'>
-                  {inject.title}
-                </Typography>
-                <CountdownChip target={new Date(inject.end_time).getTime()} />
-                {sortedSubmissions.length > 0 && (
-                  <Chip
-                    label={`Score: ${
-                      sortedSubmissions[0].rubric?.fields.reduce(
-                        (acc, field) => acc + field.score,
-                        0
-                      ) ?? 0
-                    }/${inject.rubric.max_score}`}
-                    color='success'
-                    size='small'
-                    sx={{ marginLeft: "12px" }}
-                  />
-                )}
-              </Box>
-            }
-            action={
-              <Box display='flex' flexDirection='row' gap='12px'>
-                <IconButton>
-                  {expanded ? <ExpandLess /> : <ExpandMore />}
-                </IconButton>
-                <Slide
-                  in={expanded}
-                  timeout={300}
-                  style={{
-                    transformOrigin: "right",
-                  }}
-                  direction='left'
-                  unmountOnExit
-                  mountOnEnter
-                >
-                  <Button
-                    variant='contained'
-                    color='success'
-                    onClick={(e) => {
-                      if (!expanded) {
-                        e.stopPropagation();
-                      }
-
-                      setOpen(true);
-                    }}
-                  >
-                    Submit
-                  </Button>
-                </Slide>
-              </Box>
-            }
-            onClick={handleExpandClick}
-          />
-          {expanded && <Divider sx={{ margin: "0px 20%" }} />}
-
-          <Collapse in={expanded} timeout={300}>
-            <CardContent>
-              <Typography variant='h4' align='center'>
-                Files
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexWrap: "wrap",
-                  justifyContent: "center",
-                  mt: "8px",
-                  gap: "8px",
-                  mb: "24px",
-                }}
-              >
-                {inject.files.length ? (
-                  inject.files.map((file) => (
-                    <FileChip key={file.id} file={file} />
-                  ))
-                ) : (
-                  <Typography variant='body1' align='center'>
-                    No Files
-                  </Typography>
-                )}
-              </Box>
-              <Typography variant='h4' align='center'>
-                Rubric
-              </Typography>
+          Submit
+        </Button>,
+      ]}
+      expanded={expanded}
+      setExpanded={setExpanded}
+      visible={visible}
+    >
+      <Typography variant='h4' align='center'>
+        Files
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          mt: "8px",
+          gap: "8px",
+          mb: "24px",
+        }}
+      >
+        {inject.files.length ? (
+          inject.files.map((file) => <FileChip key={file.id} file={file} />)
+        ) : (
+          <Typography variant='body1' align='center'>
+            No Files
+          </Typography>
+        )}
+      </Box>
+      <Typography variant='h4' align='center'>
+        Rubric
+      </Typography>
+      <Paper
+        elevation={3}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "16px",
+          marginTop: "8px",
+          marginBottom: "24px",
+        }}
+      >
+        {inject.rubric.fields.length && (
+          <Box display='flex' flexDirection='column' gap='8px' width='100%'>
+            {inject.rubric.fields.map((field) => (
               <Paper
-                elevation={3}
+                elevation={4}
                 sx={{
                   display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
                   alignItems: "center",
-                  padding: "16px",
-                  marginTop: "8px",
-                  marginBottom: "24px",
+                  padding: "12px",
+                  width: "100%",
+                  marginBottom: "8px",
+                  gap: "16px",
                 }}
               >
-                {inject.rubric.fields.length && (
-                  <Box
-                    display='flex'
-                    flexDirection='column'
-                    gap='8px'
-                    width='100%'
-                  >
-                    {inject.rubric.fields.map((field) => (
-                      <Paper
-                        elevation={4}
-                        sx={{
-                          display: "flex",
-                          flexDirection: "row",
-                          justifyContent: "space-between",
-                          alignItems: "center",
-                          padding: "12px",
-                          width: "100%",
-                          marginBottom: "8px",
-                          gap: "16px",
-                        }}
-                      >
-                        <TextField
-                          size='small'
-                          label='Criteria Name'
-                          value={field.name}
-                          fullWidth={sortedSubmissions.length == 0}
-                        />
-                        {sortedSubmissions.length > 0 && (
-                          <>
-                            <TextField
-                              size='small'
-                              label='Submission Notes'
-                              value={
-                                sortedSubmissions[0].rubric?.fields.find(
-                                  (f) => f.name === field.name
-                                )?.notes
-                              }
-                              fullWidth
-                            />
-                            <TextField
-                              size='small'
-                              label='Submission Score'
-                              value={
-                                sortedSubmissions[0].rubric?.fields.find(
-                                  (f) => f.name === field.name
-                                )?.score
-                              }
-                            />
-                          </>
-                        )}
-                        <TextField
-                          size='small'
-                          label='Criteria Max Points'
-                          value={field.max_score}
-                        />
-                      </Paper>
-                    ))}
-                    {sortedSubmissions.length > 0 && (
-                      <TextField
-                        size='small'
-                        label='Inject Notes'
-                        value={sortedSubmissions[0].rubric?.notes}
-                        fullWidth
-                      />
-                    )}
-                    <Box
-                      display='flex'
-                      flexDirection='row'
-                      gap='8px'
-                      marginTop='8px'
-                    >
-                      {sortedSubmissions.length > 0 && (
-                        <TextField
-                          size='small'
-                          label='Total Score'
-                          value={sortedSubmissions[0].rubric?.fields.reduce(
-                            (acc, field) => acc + field.score,
-                            0
-                          )}
-                          fullWidth
-                        />
-                      )}
-                      <TextField
-                        size='small'
-                        label='Inject Max Points'
-                        value={inject.rubric.max_score}
-                        fullWidth
-                      />
-                    </Box>
-                  </Box>
-                )}
-              </Paper>
-              <Typography variant='h4' align='center'>
-                Submissions
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  flexWrap: "wrap",
-                  justifyContent: "center",
-                  mt: "8px",
-                  gap: "8px",
-                }}
-              >
-                {inject.submissions.length ? (
+                <TextField
+                  size='small'
+                  label='Criteria Name'
+                  value={field.name}
+                  fullWidth={sortedSubmissions.length == 0}
+                />
+                {sortedSubmissions.length > 0 && (
                   <>
-                    {[...inject.submissions]
-                      .sort(
-                        (a, b) =>
-                          new Date(b.create_time).getTime() -
-                          new Date(a.create_time).getTime()
-                      )
-                      .map((submission) => (
-                        <Submission inject={inject} submission={submission} />
-                      ))}
+                    <TextField
+                      size='small'
+                      label='Submission Notes'
+                      value={
+                        sortedSubmissions[0].rubric?.fields.find(
+                          (f) => f.name === field.name
+                        )?.notes
+                      }
+                      fullWidth
+                    />
+                    <TextField
+                      size='small'
+                      label='Submission Score'
+                      value={
+                        sortedSubmissions[0].rubric?.fields.find(
+                          (f) => f.name === field.name
+                        )?.score
+                      }
+                    />
                   </>
-                ) : (
-                  <Typography variant='body1' align='center'>
-                    No Submissions
-                  </Typography>
                 )}
-              </Box>
-            </CardContent>
-          </Collapse>
-        </Card>
-      </Grow>
-    </>
+                <TextField
+                  size='small'
+                  label='Criteria Max Points'
+                  value={field.max_score}
+                />
+              </Paper>
+            ))}
+            {sortedSubmissions.length > 0 && (
+              <TextField
+                size='small'
+                label='Inject Notes'
+                value={sortedSubmissions[0].rubric?.notes}
+                fullWidth
+              />
+            )}
+            <Box display='flex' flexDirection='row' gap='8px' marginTop='8px'>
+              {sortedSubmissions.length > 0 && (
+                <TextField
+                  size='small'
+                  label='Total Score'
+                  value={sortedSubmissions[0].rubric?.fields.reduce(
+                    (acc, field) => acc + field.score,
+                    0
+                  )}
+                  fullWidth
+                />
+              )}
+              <TextField
+                size='small'
+                label='Inject Max Points'
+                value={inject.rubric.max_score}
+                fullWidth
+              />
+            </Box>
+          </Box>
+        )}
+      </Paper>
+      <Typography variant='h4' align='center'>
+        Submissions
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          mt: "8px",
+          gap: "8px",
+        }}
+      >
+        {inject.submissions.length ? (
+          <>
+            {[...inject.submissions]
+              .sort(
+                (a, b) =>
+                  new Date(b.create_time).getTime() -
+                  new Date(a.create_time).getTime()
+              )
+              .map((submission) => (
+                <Submission inject={inject} submission={submission} />
+              ))}
+          </>
+        ) : (
+          <Typography variant='body1' align='center'>
+            No Submissions
+          </Typography>
+        )}
+      </Box>
+    </Dropdown>
   );
 }
